2b27ed8d85261bdb9dbb06557822f0d794c5edde
Smoofy19 <nxcoop19@gmail.com>
2023-11-24 16:31:43 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+}
+
+group = "de.smoofy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://nexus.velocitypowered.com/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+
+    compileOnly(platform("eu.cloudnetservice.cloudnet:bom:4.0.0-RC9"))
+    compileOnly("eu.cloudnetservice.cloudnet:driver")
+    compileOnly("eu.cloudnetservice.cloudnet:bridge")
+    compileOnly("eu.cloudnetservice.cloudnet:wrapper-jvm")
+    compileOnly("eu.cloudnetservice.cloudnet:syncproxy")
+    compileOnly("eu.cloudnetservice.cloudnet:platform-inject-api")
+    compileOnly("eu.cloudnetservice.cloudnet:platform-inject-processor")
+
+    compileOnly("org.fusesource.jansi:jansi:2.4.0")
+}
+
+tasks.withType<JavaCompile>().configureEach() {
+    options.encoding = "UTF-8"
+    sourceCompatibility = JavaVersion.VERSION_17.toString()
+    targetCompatibility = JavaVersion.VERSION_17.toString()
+}
