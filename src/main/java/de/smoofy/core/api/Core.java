4b3251f9da8394733c9f6b27189dded6005ae2ea
Smoofy19 <nxcoop19@gmail.com>
2023-11-24 22:21:33 +0100

    Added methods

--- src/main/java/de/smoofy/core/api/Core.java
+++ src/main/java/de/smoofy/core/api/Core.java
@@ -8,6 +8,27 @@ package de.smoofy.core.api;
  * Created - 24.11.2023 - 17:27
  */
 
+import de.smoofy.core.api.cloud.ICloudManager;
+import de.smoofy.core.api.coins.CoinAPI;
+import de.smoofy.core.api.fetcher.IUUIDFetcher;
+import de.smoofy.core.api.game.IGameHandler;
+import de.smoofy.core.api.game.countdown.ICountdownManager;
+import de.smoofy.core.api.game.map.IWorldManager;
+import de.smoofy.core.api.game.phase.IPhaseManager;
+import de.smoofy.core.api.logger.ILogger;
+import de.smoofy.core.api.message.ITextBuilder;
+import de.smoofy.core.api.modules.actionbar.IActionbar;
+import de.smoofy.core.api.modules.entities.IEntityHandler;
+import de.smoofy.core.api.modules.hologram.IHologramHandler;
+import de.smoofy.core.api.modules.hotbar.IHotbarHandler;
+import de.smoofy.core.api.modules.location.ILocationHandler;
+import de.smoofy.core.api.modules.particle.IParticleHandler;
+import de.smoofy.core.api.modules.skulls.ISkullHandler;
+import de.smoofy.core.api.modules.tablist.ITablistBuilder;
+import de.smoofy.core.api.modules.tasks.ICoreRunTask;
+import de.smoofy.core.api.modules.title.ITitleHandler;
+import de.smoofy.core.api.player.ICorePlayerManager;
+import de.smoofy.core.api.time.ITime;
 import lombok.Getter;
 
 /**
@@ -22,4 +43,110 @@ public abstract class Core {
         Core.instance = instance;
         return getInstance() != null;
     }
+
+    /**
+     * @return the {@link ICloudManager}
+     */
+    public abstract ICloudManager cloudManager();
+
+    /**
+     * @return the {@link CoinAPI}
+     */
+    public abstract CoinAPI coinAPI();
+
+    /**
+     * @return the {@link IUUIDFetcher}
+     */
+    public abstract IUUIDFetcher uuidFetcher();
+
+    /**
+     * @return the {@link ICountdownManager}
+     */
+    public abstract ICountdownManager countdownManager();
+
+    /**
+     * @return the {@link IWorldManager}
+     */
+    public abstract IWorldManager worldManager();
+
+    /**
+     * @return the {@link IPhaseManager}
+     */
+    public abstract IPhaseManager phaseManager();
+
+    /**
+     * @return the {@link IGameHandler}
+     */
+    public abstract IGameHandler gameHandler();
+
+    /**
+     * @return the {@link ILogger}
+     */
+    public abstract ILogger logger();
+
+    /**
+     * @param text the text
+     * @return the {@link ITextBuilder}
+     */
+    public abstract ITextBuilder textBuilder(String text);
+
+    /**
+     * @return the {@link IActionbar}
+     */
+    public abstract IActionbar actionbar();
+
+    /**
+     * @return the {@link IEntityHandler}
+     */
+    public abstract IEntityHandler entityHandler();
+
+    /**
+     * @return the {@link IHologramHandler}
+     */
+    public abstract IHologramHandler hologramHandler();
+
+    /**
+     * @return the {@link IHotbarHandler}
+     */
+    public abstract IHotbarHandler hotbarHandler();
+
+    /**
+     * @return the {@link ILocationHandler}
+     */
+    public abstract ILocationHandler locationHandler();
+
+    /**
+     * @return the {@link IParticleHandler}
+     */
+    public abstract IParticleHandler particleHandler();
+
+    /**
+     * @return the {@link ISkullHandler}
+     */
+    public abstract ISkullHandler skullHandler();
+
+    /**
+     * @return the {@link ITablistBuilder}
+     */
+    public abstract ITablistBuilder tablistBuilder();
+
+    /**
+     * @return the {@link ICoreRunTask}
+     */
+    public abstract ICoreRunTask runTask();
+
+    /**
+     * @return the {@link ITitleHandler}
+     */
+    public abstract ITitleHandler titleHandler();
+
+    /**
+     * @return the {@link ICorePlayerManager}
+     */
+    public abstract ICorePlayerManager corePlayerManager();
+
+    /**
+     * @return the {@link ITime}
+     */
+    public abstract ITime time();
 }
