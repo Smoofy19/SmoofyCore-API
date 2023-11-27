e293a536831726d80bfaf8792120e84d40c1b4df
Smoofy19 <nxcoop19@gmail.com>
2023-11-27 12:20:41 +0100

    Initial commit

---
+++ src/main/java/de/smoofy/core/api/modules/schematic/ISchematic.java
@@ -0,0 +1,21 @@
+package de.smoofy.core.api.modules.schematic;
+/*
+ * Copyright ©
+ * @author - Smoofy
+ * @GitHub - https://github.com/Smoofy19
+ * @Twitter - https://twitter.com/Smoofy19
+ * Created - 24.11.2023 - 17:27
+ */
+
+import org.bukkit.Location;
+
+/**
+ *
+ */
+
+public interface ISchematic {
+
+    Location first();
+
+    Location second();
+}
