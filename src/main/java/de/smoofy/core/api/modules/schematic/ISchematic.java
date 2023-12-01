6827de835ebb14d36ee9e851cb16a3fb733719f1
Smoofy19 <nxcoop19@gmail.com>
2023-12-01 13:25:15 +0100

    Updated method

--- src/main/java/de/smoofy/core/api/modules/schematic/ISchematic.java
+++ src/main/java/de/smoofy/core/api/modules/schematic/ISchematic.java
@@ -22,7 +22,8 @@ public interface ISchematic {
      * @param type     the type (e.g. 8x1)
      * @param name     the name of the map
      * @param location the start location
+     * @param async    if the schematic should be pasted async with FAWE
      * @return true if the schematic was pasted successfully, otherwise false
      */
-    boolean paste(String game, String type, String name, Location location);
+    boolean paste(String game, String type, String name, Location location, boolean async);
 }
