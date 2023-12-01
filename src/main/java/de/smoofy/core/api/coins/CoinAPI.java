a8726a1cbb687b97ca151a786cd32c8441ea5d6d
Smoofy19 <nxcoop19@gmail.com>
2023-12-01 13:14:46 +0100

    Added methods

--- src/main/java/de/smoofy/core/api/coins/CoinAPI.java
+++ src/main/java/de/smoofy/core/api/coins/CoinAPI.java
@@ -8,8 +8,42 @@ package de.smoofy.core.api.coins;
  * Created - 24.11.2023 - 17:27
  */
 
+import java.util.UUID;
+
 /**
  *
  */
 public interface CoinAPI {
+
+    /**
+     * Get the amount of coins of a player.
+     *
+     * @param uuid The UUID of the player
+     * @return the amount of coins
+     */
+    int coins(UUID uuid);
+
+    /**
+     * Add coins to a player.
+     *
+     * @param uuid The UUID of the player
+     * @param amount The amount of coins
+     */
+    void add(UUID uuid, int amount);
+
+    /**
+     * Remove coins from a player.
+     *
+     * @param uuid The UUID of the player
+     * @param amount The amount of coins
+     */
+    void remove(UUID uuid, int amount);
+
+    /**
+     * Set the amount of coins of a player.
+     *
+     * @param uuid The UUID of the player
+     * @param amount The amount of coins
+     */
+    void set(UUID uuid, int amount);
 }
