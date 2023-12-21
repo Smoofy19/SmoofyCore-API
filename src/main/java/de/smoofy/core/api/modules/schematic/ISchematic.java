d6c49131fe9e446fd8829d6454bddd7137246474
Smoofy19 <nxcoop19@gmail.com>
2023-12-21 23:04:10 +0100

    Added methods

--- src/main/java/de/smoofy/core/api/modules/schematic/ISchematic.java
+++ src/main/java/de/smoofy/core/api/modules/schematic/ISchematic.java
@@ -8,6 +8,9 @@ package de.smoofy.core.api.modules.schematic;
  */
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import java.util.List;
 
 /**
  *
@@ -22,7 +25,29 @@ public interface ISchematic {
      * @param type     the type (e.g. 8x1)
      * @param name     the name of the map
      * @param location the start location
+     * @param fawe     if the schematic should be pasted with FastAsyncWorldEdit
      * @return true if the schematic was pasted successfully, otherwise false
      */
-    boolean paste(String game, String type, String name, Location location);
+    boolean paste(String game, String type, String name, Location location, boolean fawe);
+
+    /**
+     * Save a schematic.
+     *
+     * @param game   the game (e.g. SkyWars)
+     * @param type   the type (e.g. 8x1)
+     * @param name   the name of the map
+     * @param blocks the blocks
+     * @return true if the schematic was saved successfully, otherwise false
+     */
+    boolean save(String game, String type, String name, List<Block> blocks);
+
+    /**
+     * Delete a schematic.
+     *
+     * @param game the game (e.g. SkyWars)
+     * @param type the type (e.g. 8x1)
+     * @param name the name of the map
+     * @return true if the schematic was deleted successfully, otherwise false
+     */
+    boolean delete(String game, String type, String name);
 }
